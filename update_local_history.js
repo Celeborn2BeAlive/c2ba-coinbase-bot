87abb5f2d0101b20bcf7dcf9df213857b3207408
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2019-02-05 16:26:10 +0100

    Improve code readability by using lodash and aigle

--- update_local_history.js
+++ update_local_history.js
@@ -1,6 +1,8 @@
 const jsonfile = require('jsonfile-promised')
 const fs = require('fs')
 const Gdax = require('gdax')
+const Aigle = require('aigle')
+const { flow, filter, map, chain, union, uniq, curry } = Aigle.mixin(require('lodash'))
 const { coinbaseURI, getAccountHistory, arrayToObject } = require('./utils')
 
 const { api: apiURI } = coinbaseURI
@@ -24,10 +26,12 @@ async function main() {
     const authedClient = new Gdax.AuthenticatedClient(apiKey, apiSecret, passPhrase, apiURI)
     const accounts = await authedClient.getAccounts()
 
-    const updatedAccounts = (await Promise.all(accounts.map(async account => {
+    const getAccountHistoryAndTransfers = async account => {
+        const registeredTransfers = registeredAccounts[account.id] ? registeredAccounts[account.id].transfers : {}
+        const coinbaseTransfers = arrayToObject(await authedClient.getAccountTransfers(account.id), "id")
         const transfers = {
-            ...(registeredAccounts[account.id] ? registeredAccounts[account.id].transfers : {}),
-            ...arrayToObject(await authedClient.getAccountTransfers(account.id), "id")
+            ...registeredTransfers,
+            ...coinbaseTransfers
         }
         const currentHistory = registeredAccounts[account.id] ? registeredAccounts[account.id].history : []
         const history = await getAccountHistory(authedClient, account.id, currentHistory)
@@ -36,18 +40,35 @@ async function main() {
             history,
             transfers
         }
-    }))).filter(account => account.history.length > 0 || Object.keys(account.transfers).length > 0)
+    }
 
-    const allOrders = [...new Set(updatedAccounts.map(account => {
-        return account.history.filter(event => "order_id" in event.details).map(event => event.details["order_id"])
-    }).reduce((previous, current) => previous.concat(current), []))]
+    const updatedAccounts = await
+        chain(accounts)
+            .map(getAccountHistoryAndTransfers)
+            .filter(account => { return account.history.length > 0 || Object.keys(account.transfers).length > 0 })
+
+    const getAccountOrderIds = account =>
+        account.history
+            .filter(event => "order_id" in event.details)
+            .map(event => event.details["order_id"])
+
+    const allOrders = await
+        chain(updatedAccounts)
+            .map(getAccountOrderIds)
+            .reduce((prev, curr) => curr.concat(prev))
+            .uniq()
 
     const currentOrders = currentHistory.orders
     const ordersToRequest = allOrders.filter(order => !(order in currentOrders))
-    const newOrders = arrayToObject(await Promise.all(ordersToRequest.map(async (orderId, index) => {
-        console.log(`Requesting order ${orderId} (${index}/${ordersToRequest.length - 1})`)
-        return await authedClient.getOrder(orderId)
-    })), "id")
+
+    const newOrders = await
+        chain(allOrders)
+            .filter(order => !(order in currentOrders))
+            .map((orderId, index) => {
+                console.log(`Requesting order ${orderId} (${index}/${ordersToRequest.length - 1})`)
+                return authedClient.getOrder(orderId)
+            })
+            .thru(orders => arrayToObject(orders, "id"))
 
     const formatedHistory = {
         accounts: arrayToObject(updatedAccounts, 'id'),
