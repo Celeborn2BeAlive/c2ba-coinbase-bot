4cb5112f33bf0658f03363c4cce626c68fc0be30
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2019-02-07 13:11:56 +0100

    Improve code

--- update_local_history.js
+++ update_local_history.js
@@ -7,6 +7,13 @@ const { coinbaseURI, getAccountHistory, arrayToObject } = require('./utils')
 
 const { api: apiURI } = coinbaseURI
 
+class CoinbaseGlobalHistory {
+    constructor() {
+        this.accounts = {} // All accounts, identified by their coinbase ID
+        this.orders = {} // All orders referenced by account history, identified by their coinbase ID
+    }
+}
+
 async function main() {
     const pathToConfig = process.argv[2]
     if (!pathToConfig) {
@@ -18,16 +25,19 @@ async function main() {
         throw new Error("No path to local history file provided.")
     }
 
-    const currentHistory = fs.existsSync(pathToLocalHistory) ? await jsonfile.readFile(pathToLocalHistory) : { accounts: {}, orders: {} }
-    const registeredAccounts = currentHistory.accounts
+    const previousHistory = fs.existsSync(pathToLocalHistory) ?
+        await jsonfile.readFile(pathToLocalHistory) :
+        { accounts: {}, orders: {} }
+    const registeredAccounts = previousHistory.accounts
 
     const { apiKey, apiSecret, passPhrase } = await jsonfile.readFile(pathToConfig)
 
     const authedClient = new Gdax.AuthenticatedClient(apiKey, apiSecret, passPhrase, apiURI)
-    const accounts = await authedClient.getAccounts()
 
     const getAccountHistoryAndTransfers = async account => {
-        const registeredTransfers = registeredAccounts[account.id] ? registeredAccounts[account.id].transfers : {}
+        const registeredTransfers = registeredAccounts[account.id] ?
+            registeredAccounts[account.id].transfers :
+            {}
         const coinbaseTransfers = arrayToObject(await authedClient.getAccountTransfers(account.id), "id")
         const transfers = {
             ...registeredTransfers,
@@ -42,33 +52,26 @@ async function main() {
         }
     }
 
-    const updatedAccounts = await
-        chain(accounts)
-            .map(getAccountHistoryAndTransfers)
-            .filter(account => { return account.history.length > 0 || Object.keys(account.transfers).length > 0 })
+    const updatedAccounts = await chain(authedClient.getAccounts())
+        .map(getAccountHistoryAndTransfers)
+        .filter(account => { return account.history.length > 0 || Object.keys(account.transfers).length > 0 })
 
-    const getAccountOrderIds = account =>
+    const getOrderIdsOfAccount = account =>
         account.history
             .filter(event => "order_id" in event.details)
-            .map(event => event.details["order_id"])
+            .map(event => event.details.order_id)
 
-    const allOrders = await
-        chain(updatedAccounts)
-            .map(getAccountOrderIds)
-            .reduce((prev, curr) => curr.concat(prev))
-            .uniq()
-
-    const currentOrders = currentHistory.orders
-    const ordersToRequest = allOrders.filter(order => !(order in currentOrders))
-
-    const newOrders = await
-        chain(allOrders)
-            .filter(order => !(order in currentOrders))
-            .map((orderId, index) => {
-                console.log(`Requesting order ${orderId} (${index}/${ordersToRequest.length - 1})`)
-                return authedClient.getOrder(orderId)
-            })
-            .thru(orders => arrayToObject(orders, "id"))
+    const currentOrders = previousHistory.orders
+    const newOrders = await chain(updatedAccounts)
+        .map(getOrderIdsOfAccount)
+        .reduce((prev, curr) => curr.concat(prev))
+        .uniq()
+        .filter(order => !(order in currentOrders))
+        .map((orderId, index, ordersToRequest) => {
+            console.log(`Requesting order ${orderId} (${index}/${ordersToRequest.length - 1})`)
+            return authedClient.getOrder(orderId)
+        })
+        .thru(orders => arrayToObject(orders, "id"))
 
     const formatedHistory = {
         accounts: arrayToObject(updatedAccounts, 'id'),
